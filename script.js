556f397e961d55704164bca741ad4e93f75b4619
clintj42 <clintj42@gmail.com>
2016-01-01 14:54:04 -0700

    New drag

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ var moveList = [];
 var sections = [];
 var dragging = false;
 
+$(document).on("click touchend", ".card", function(e){
+	if(!dragging) {
+		$(this).css("z-index", "100");
+		$(this).css("transform", "0");
+		$(this).attr("data-x", "0");
+		$(this).attr("data-y", "0");
+		clickCard($(this));
+	} else {
+		dragging = false;
+	}
+});
+
 $(document).ready(function() {
 	if(window.location.href.indexOf("tutorial2") !== -1 || window.location.href.indexOf("tutorial3") !== -1) {
 		freeDiscards = 0;
@@ -23,74 +35,77 @@ $(document).ready(function() {
 		draw();
 	});
 	
-	interact('.card')
-  .draggable({
-    // enable inertial throwing
-    inertia: true,
-    // keep the element within the area of it's parent
-    restrict: {
-      restriction: "parent",
-      endOnly: true,
-      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-    },
-    // enable autoScroll
-    autoScroll: true,
-
-    // call this function on every dragmove event
-    onmove: dragMoveListener,
-    // call this function on every dragend event
-    onend: function (event) {
-      var textEl = event.target.querySelector('p');
-
-      textEl && (textEl.textContent =
-        'moved a distance of '
-        + (Math.sqrt(event.dx * event.dx +
-                     event.dy * event.dy)|0) + 'px');
-    }
-  });
+	interact('.card:last-child').draggable({
+		inertia: false,
+		restrict: {
+			restriction: document.getElementById("body"),
+			endOnly: true,
+			elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+		},
+		autoScroll: false,
+		onmove: dragMoveListener
+	});
 	
+	interact("#body").dropzone({
+		accept: ".card",
+		overlap: 0.75,
+		ondrop: function(event) {
+			event.relatedTarget.style.zIndex = 100;
+			event.relatedTarget.style.transform = "none";
+			event.relatedTarget.setAttribute("data-x", 0);
+			event.relatedTarget.setAttribute("data-y", 0);
+		}
+	});
 
-	// enable draggables to be dropped into this
 	interact('.cardSection').dropzone({
-	  // only accept elements matching this CSS selector
-	  accept: '.card',
-	  // Require a 75% element overlap for a drop to be possible
-	  overlap: 0.75,
-
-	  // listen for drop related events:
-
-	  ondropactivate: function (event) {
-		// add active dropzone feedback
-		//event.target.classList.add('drop-active');
-		console.log("Drop Activate");
-	  },
-	  ondragenter: function (event) {
-		// var draggableElement = event.relatedTarget,
-			// dropzoneElement = event.target;
-
-		// // feedback the possibility of a drop
-		// dropzoneElement.classList.add('drop-target');
-		// draggableElement.classList.add('can-drop');
-		// draggableElement.textContent = 'Dragged in';
-		console.log("Drag Enter")
-	  },
-	  ondragleave: function (event) {
-		// // remove the drop feedback style
-		// event.target.classList.remove('drop-target');
-		// event.relatedTarget.classList.remove('can-drop');
-		// event.relatedTarget.textContent = 'Dragged out';
-		console.log("Drag leave");
-	  },
-	  ondrop: function (event) {
-		//event.relatedTarget.textContent = 'Dropped';
-		console.log("Drop")
-	  },
-	  ondropdeactivate: function (event) {
-		// // remove active dropzone feedback
-		// event.target.classList.remove('drop-active');
-		// event.target.classList.remove('drop-target');
-		console.log("Drop Deactivate");
-	  }
+		accept: '.card',
+		overlap: 0.75,
+		ondrop: function (event) {
+			event.relatedTarget.style.zIndex = 100;
+			event.relatedTarget.style.transform = "none";
+			event.relatedTarget.setAttribute("data-x", 0);
+			event.relatedTarget.setAttribute("data-y", 0);
+			
+			console.log(event.target.getAttribute("id"));
+			var sectionDragTo = parseInt(event.target.getAttribute("id").replace("section","")) - 1;
+			var sectionDragFrom = parseInt(event.relatedTarget.parentElement.getAttribute("id").replace("section","")) - 1;
+			if(sectionDragFrom === sectionDragTo) {
+				clickCard(jQuery(event.relatedTarget));
+				return;
+			}
+			
+			//Dragging
+			if(window.location.href.indexOf("tutorial2") === -1){
+				var isEmpty = true;
+				var jqueryTarget = jQuery(event.target);
+				jqueryTarget.children().each(function() {
+					console.log($(this).attr("value"));
+					if($(this).attr("value") !== "") {
+						isEmpty = false;
+					}
+				});
+				if(isEmpty) {
+					var card = sections[sectionDragFrom].shift();
+					sections[sectionDragTo].unshift(card);
+					displayCards();
+					moveList.push("drag|" + event.relatedTarget.parentElement.getAttribute("id") + "|" + event.target.getAttribute("id").replace("section",""));
+					checkEndGame();
+					if(window.location.href.indexOf("tutorial3") !== -1) {
+						$("#instruction2").text("Great job!");
+						$("#instruction2").show();
+						$('html, body').animate({
+							scrollTop: $("#instruction2").offset().top
+						}, 2000);
+					}
+				} else if(window.location.href.indexOf("tutorial") !== -1) {
+					$("#instruction2").text("You cannot drag to a pile unless it is empty.");
+					$("#instruction2").show();
+					$('html, body').animate({
+						scrollTop: $("#instruction2").offset().top
+					}, 2000);
+				}
+			}	
+		}
 	});
 	
 	
@@ -176,29 +191,26 @@ $(document).ready(function() {
 
 
 
-  function dragMoveListener (event) {
+function dragMoveListener (event) {
+	dragging = true;
+	event.target.style.zIndex = 999;
     var target = event.target,
-        // keep the dragged position in the data-x/data-y attributes
-        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+		// keep the dragged position in the data-x/data-y attributes
+		x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+		y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
     // translate the element
     target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
+		target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
 
     // update the posiion attributes
     target.setAttribute('data-x', x);
     target.setAttribute('data-y', y);
-  }
+}
 
-  // this is used later in the resizing and gesture demos
-  window.dragMoveListener = dragMoveListener;
+// this is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener;
 
-
-$(document).on("click", ".card", function(){
-	clickCard($(this));
-});
 
 function undo() {
 	if(moveList.length > 1) {
